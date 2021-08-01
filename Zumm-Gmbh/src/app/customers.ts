e4b13caec224ac03c8335f2a557d5ed11bce7d84
markus kamitz <markus_kamitz@yahoo.de>
2021-08-01 12:16:09 +0200

    customers dist repair

--- Zumm-Gmbh/src/app/customers.ts
+++ Zumm-Gmbh/src/app/customers.ts
@@ -1,51 +1,63 @@
 export const customers = [
   {
     name: "MegaCorp", 
-    customer: "customer1"
+    customer: "customer1",
+    bgPic: "assets/company1.jpeg",
   },
   {
     name: "Wayne Industries", 
-    customer: "customer2"
+    customer: "customer2",
+    bgPic: "assets/company2.jpeg",
   },
   {
     name: "CosyOffices", 
-    customer: "customer3"
+    customer: "customer3",
+    bgPic: "assets/company3.webp",
   },
   {
     name: "Green Planet", 
-    customer: "customer4"
+    customer: "customer4",
+    bgPic: "assets/company4.jpeg",
   },
   {
     name: "CoWorking", 
-    customer: "customer5"
+    customer: "customer5",
+    bgPic: "assets/company5.webp",
   },
   {
     name: "ShareSpace", 
-    customer: "customer6"
+    customer: "customer6",
+    bgPic: "assets/company6.webp",
   },
   {
     name: "Bank of Banks", 
-    customer: "customer7"
+    customer: "customer7",
+    bgPic: "assets/company7.webp",
   },
   {
     name: "CoSpace", 
-    customer: "customer8"
+    customer: "customer8",
+    bgPic: "assets/company8.jpeg",
   },
   {
     name: "S & M", 
-    customer: "customer9"
+    customer: "customer9",
+    bgPic: "assets/company9.webp",
   },
   {
     name: "The Creperie", 
-    customer: "customer10"
+    customer: "customer10",
+    bgPic: "assets/company10.jpeg",
   },
   {
     name: "Texstyle", 
-    customer: "customer11"
+    customer: "customer11",
+    bgPic: "assets/company11.jpeg",
   },
   {
     name: "Mambo Nr. 5", 
-    customer: "customer12"
+    customer: "customer12",
+    bgPic: "assets/company12.jpeg",
   },
 
 ]
